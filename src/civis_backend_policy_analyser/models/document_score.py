03051755689fefa9e99e3b1c59a527a7d2e5348f
Shreenidhi Nayak <codeshreenidhi@gmail.com>
2025-07-16 21:13:24 +0530

    model and schema changes

--- src/civis_backend_policy_analyser/models/document_score.py
+++ src/civis_backend_policy_analyser/models/document_score.py
@@ -1,13 +1,15 @@
-from sqlalchemy import Column, String, Integer, TIMESTAMP, JSON, func, ForeignKey
+from sqlalchemy import Column, String, Text, Integer, Float, TIMESTAMP, func, ForeignKey
 from civis_backend_policy_analyser.models.base import Base
 
 class DocumentScore(Base):
     __tablename__ = "document_score"
 
     score_id = Column(Integer, primary_key=True, autoincrement=True)
-    doc_id = Column(String(100), ForeignKey('document_metadata.doc_id', on_delete='CASCADE'), nullable=False)
+    doc_id = Column(String(100), ForeignKey('document_metadata.doc_id', ondelete='CASCADE'), nullable=False)
     assessment_id = Column(Integer, ForeignKey('assessment_area.assessment_id', ondelete='CASCADE'), nullable=False)
     prompt_id = Column(Integer, ForeignKey('prompt.prompt_id', ondelete='CASCADE'), nullable=False)
-    assessment_json = Column(JSON)
+    prompt_score = Column(Float, nullable=False)
+    max_score = Column(Integer, nullable=False)
+    score_justification = Column(Text, nullable=False)
     created_on = Column(TIMESTAMP, default=func.now())
     created_by = Column(String(100))
--- src/civis_backend_policy_analyser/schemas/document_score_schema.py
+++ src/civis_backend_policy_analyser/schemas/document_score_schema.py
@@ -2,21 +2,22 @@ from civis_backend_policy_analyser.schemas.base_model import BaseModelSchema
 from typing import List, Optional
 from datetime import datetime
 
-class PromptJustification(BaseModelSchema):
-    prompt_summary: str
-    prompt_justification: str
+class PromptAnalysis(BaseModelSchema):
+    description: str
+    justification: str
 
 class DocumentScoreOut(BaseModelSchema):
-    assessment_area_name: str
     score: float
-    prompt_analysis: List[PromptJustification]
+    prompt_analysis: List[PromptAnalysis]
 
 class DocumentScoreSchema(BaseModelSchema):
     score_id: Optional[int] = None
     doc_id: str
     assessment_id: str
     prompt_id: str
-    assessment_json: dict
+    prompt_score: float
+    max_score: int
+    score_justification: str
     created_on: Optional[datetime] = None
     created_by: Optional[str] = None
 
