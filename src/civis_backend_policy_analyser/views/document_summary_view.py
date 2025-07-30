dcc83216f07850efb852ea8a83f81c3b9569ff1f
Shreenidhi Nayak <codeshreenidhi@gmail.com>
2025-07-30 07:28:08 +0530

    updated summary step to pick sthe prompt with prompt type 'DOCUMENT_SUMMARY'  from prompt table

--- src/civis_backend_policy_analyser/views/document_summary_view.py
+++ src/civis_backend_policy_analyser/views/document_summary_view.py
@@ -8,9 +8,11 @@ from civis_backend_policy_analyser.core.embeddings.ollama_embedding import Ollam
 from civis_backend_policy_analyser.core.llm.azure_llm import AzureLLMModel
 from civis_backend_policy_analyser.core.llm.ollama_llm import OllamaLLMModel
 from civis_backend_policy_analyser.models.document_summary import DocumentSummary
+from civis_backend_policy_analyser.models.prompt import Prompt
 from civis_backend_policy_analyser.schemas.document_summary_schema import DocumentSummaryResponseSchema, DocumentSummarySchema
 from civis_backend_policy_analyser.utils.constants import LLM_CLIENT
 from civis_backend_policy_analyser.views.base_view import BaseView
+from civis_backend_policy_analyser.views.prompt_view import PromptView
 
 
 class DocumentSummaryView(BaseView):
@@ -22,11 +24,16 @@ class DocumentSummaryView(BaseView):
         document_summary: DocumentSummary = await self.get(doc_summary_id)
         if not document_summary:
             raise ValueError(f"DocumentSummary with ID {doc_summary_id} not found")
+        
+        prompt_view = PromptView(self.db_session)
+        summary_prompt: Prompt = await prompt_view.filter(prompt_type = 'DOCUMENT_SUMMARY')[0]
+        if not summary_prompt:
+            raise ValueError(f"Document Summary prompt not found in prompt table")
 
         agent = create_document_agent(client=LLMClient(LLM_CLIENT), document_id=document_summary.doc_id)
 
         logger.info(f"started fetching summary from LLM for document id: {document_summary.doc_id}")
-        summary = agent.summarize()
+        summary = agent.summarize(summary_prompt=summary_prompt.technical_prompt)
         logger.info(f"fetched summary from LLM: {summary}")
         
         if not summary:
